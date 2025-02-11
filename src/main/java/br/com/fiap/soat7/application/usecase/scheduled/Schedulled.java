d0e0015b4b2292ba1a2841f9a32bc23a1d803d9c
Leandro Shiniti Tacara <leandro.tacara@hotmail.com>
2025-02-10 23:13:20 -0300

    add workflow

--- src/main/java/br/com/fiap/soat7/application/usecase/scheduled/Schedulled.java
+++ src/main/java/br/com/fiap/soat7/application/usecase/scheduled/Schedulled.java
@@ -2,16 +2,19 @@ package br.com.fiap.soat7.application.usecase.scheduled;
 
 import br.com.fiap.soat7.application.service.ProcessVideoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class Schedulled {
     private final ProcessVideoService processVideoService;
 
     @Scheduled(fixedDelay = 3000)
     public void executeProcessVideo() {
+        log.info("Iniciando processamento de video...");
         processVideoService.processQueue();
     }
 
